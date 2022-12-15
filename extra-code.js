8b6543ce7895adba22c6affbe4fa075c49564b0f
David Arcos <arcosd@gmail.com>
2022-12-15 15:14:15 -0500

    Add files via upload

---
+++ extra-code.js
@@ -0,0 +1,14 @@
+ // axios.get("https://www.theguardian.com/environment/climate-crisis")
+   // .then((response) => {
+   //    const html = response.data
+   //    const $ = cheerio.load('html')
+   //    $('a:contains("climate")', html).each(function(){
+   //       const title = $(this).text()
+   //       const url = $(this).attr('href')
+   //       articles.push({
+   //          title,
+   //          url
+   //       })
+   //    })
+   //    res.json(articles)
+   // }).catch((err => console.log(err)))
